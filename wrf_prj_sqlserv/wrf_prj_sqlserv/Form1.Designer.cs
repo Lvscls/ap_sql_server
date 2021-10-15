fb0103ace98862fd5106a85723cd7f01c0a47e7a
Vasconcelos <lilianvscls@gmail.com>
2021-10-15 16:19:13 +0200

    <feat>afficherunclient
    nous avons discuter

--- wrf_prj_sqlserv/wrf_prj_sqlserv/Form1.Designer.cs
+++ wrf_prj_sqlserv/wrf_prj_sqlserv/Form1.Designer.cs
@@ -32,6 +32,13 @@
             this.clientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.listeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ajoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btConnect = new System.Windows.Forms.Button();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -58,21 +65,80 @@
             // listeToolStripMenuItem
             // 
             this.listeToolStripMenuItem.Name = "listeToolStripMenuItem";
-            this.listeToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.listeToolStripMenuItem.Size = new System.Drawing.Size(120, 26);
             this.listeToolStripMenuItem.Text = "Liste";
             this.listeToolStripMenuItem.Click += new System.EventHandler(this.listeToolStripMenuItem_Click);
             // 
             // ajoutToolStripMenuItem
             // 
             this.ajoutToolStripMenuItem.Name = "ajoutToolStripMenuItem";
-            this.ajoutToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.ajoutToolStripMenuItem.Size = new System.Drawing.Size(120, 26);
             this.ajoutToolStripMenuItem.Text = "Ajout";
+            // 
+            // btConnect
+            // 
+            this.btConnect.Location = new System.Drawing.Point(304, 208);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(156, 73);
+            this.btConnect.TabIndex = 1;
+            this.btConnect.Text = "Connexion";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(26, 60);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(164, 22);
+            this.tbID.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(26, 170);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(26, 224);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(164, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(26, 284);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(164, 22);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(26, 348);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(164, 22);
+            this.textBox5.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.btConnect);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -91,6 +157,13 @@
         private System.Windows.Forms.ToolStripMenuItem clientToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem listeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ajoutToolStripMenuItem;
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
--- wrf_prj_sqlserv/wrf_prj_sqlserv/Form1.cs
+++ wrf_prj_sqlserv/wrf_prj_sqlserv/Form1.cs
@@ -28,5 +28,28 @@ namespace wrf_prj_sqlserv
             frm_liste_client lafenetre = new frm_liste_client();
             lafenetre.Show();
         }
+
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn1 = new SqlConnection(@"Data Source=BTS2020-29\SQLEXPRESS;Initial Catalog=SEG_V1;Integrated Security=True");
+            conn1.Open();
+            SqlCommand cmd1 = new SqlCommand("select id,nom,prenom,adresse,cp,ville from CLIENT where id=@id", conn1);
+            cmd1.Parameters.AddWithValue("id", tbID.Text);
+            SqlDataReader reader1;
+            reader1 = cmd1.ExecuteReader();
+            if (reader1.Read())
+            {
+                textBox1.Text = reader1["nom"].ToString();
+                textBox2.Text = reader1["prenom"].ToString();
+                textBox3.Text = reader1["adresse"].ToString();
+                textBox4.Text = reader1["cp"].ToString();
+                textBox5.Text = reader1["ville"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("No data found.");
+            }
+            conn1.Close();
+        }
     }
 }
